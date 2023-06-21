3563d8263d4dfd07c97a8e9c364697d648e3c744
dafbob911 <77201198+dafbob911@users.noreply.github.com>
2023-06-21 16:51:56 +0100

    correction problème mes favoris (khalid)

--- resources/views/mes_favoris.blade.php
+++ resources/views/mes_favoris.blade.php
@@ -111,9 +111,12 @@
 
 
 
-
+<div class="container">
 
   <h2 style="padding: 20px;@if(App::getlocale()=="ar") text-align:end; @endif">{{__('nav.myfavoris')}} </h2>
+
+</div>
+
 <div class="tab" >
     <button class="tablinks" onclick="openCity(event, 'Articles')">{{__('nav.articles')}}</button>
     <button class="tablinks " onclick="openCity(event, 'Boutiques')">{{__('nav.boutiques')}}</button>
@@ -210,7 +213,7 @@
                 
 
                
-                 <img id="art{{$article['idarticles']}}" onclick="delete_article({{$article['idarticles']}})"  class="topright pointer" height="30" width="30" src={{ asset('storage/supp2_new.png') }}  alt="">
+                      <img id="delete_article" onclick="delete_boutique({{$boutique['id_utilisateur']}})"  class="topright pointer" height="30" width="30" src={{ asset('storage/supp2_new.png') }} alt="">
                  
                  
 
