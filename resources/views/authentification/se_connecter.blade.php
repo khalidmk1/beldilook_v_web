496815fc9c5f386f7294472dab6982a60d367458
MedELherd <mohammed.elherd@gmail.com>
2023-06-22 07:32:53 +0200

    Formulaie page se connecter styling

--- resources/views/authentification/se_connecter.blade.php
+++ resources/views/authentification/se_connecter.blade.php
@@ -26,14 +26,154 @@
     margin-top: 5px;
   }
 }
+.form_connect_container h2 {
+  font-family:var(--font-secondary);
+  font-size:24px;
+}
+.form_connect label
+{
+  color:var(--color-primary);
+  font-weight:400;
+  letter-spacing:.1px;
+  font-size:16px;
+  font-family:var(--font-secondary);
+
+}
+
+.form_connect input{
+ border: solid var(--color-primary) 1px;
+ border-radius:10px;
+ margin-top:5px;
+ color:var(--color-primary) !important;
+ font-weight:400 !important;
+}
+
+.form_connect .mot_de_pass_oublie_txt a
+{
+  
+  position:relative;
+  bottom:10px;
+  text-decoration:none;
+  color: #bbbbbb;
+  font-size:15px;
+  font-family:var(--font-secondary);
+  
+}
+.form_connect .mot_de_pass_oublie_txt a:hover
+{
+  color:var(--color-primary);
+}
+
+.form_connect button{
+  color:#fff;
+  background-color: var(--color-primary);
+  border: solid 2px transparent;
+  border-radius:10px;
+  padding:8px;
+  margin-bottom:20px;
+}
+
+.form_connect button:hover
+{
+  background:#EFEFEF;
+  color:var(--color-primary);
+  border: solid 2px var(--color-primary);
+  transition:.2s;
+}
+
+.form_connect .fb_connect,
+.form_connect .google_connect
+{
+  color:var(--color-primary);
+  border: solid 1px #dadada;
+  border-radius:10px;
+  font-size:14px;
+  line-height:1.5rem;
+  width:240px !important;
+
+}
+.form_connect .fb_connect:hover,
+.form_connect .google_connect:hover
+{
+  color:var(--color-primary);
+  background-color: #3B5998;
+  opacity: 0.70;
+  text-decoration: none;
+}
+
+@media screen and (max-width:1160px) {
+  .form_connect_container > div:first-of-type
+  {
+    width:60% !important;
+  }
+}
+
+@media screen and (max-width:900px) {
+  .form_connect_container > div:first-of-type
+  {
+    width:60% !important;
+  }
+
+  .form_connect_container .connect_link_container
+  {
+    flex-direction:column !important;
+  }
+
+.form_connect .fb_connect,
+.form_connect .google_connect
+{
+  font-size:13px; 
+  margin-top:5px;
+  line-height:1.7rem;
+}
+}
+
+.or_separator
+{
+ color:#bbbbbb;
+ position:relative;
+ margin-top:20px;
+}
+.or_separator::before,
+.or_separator::after {
+	content: "";
+	height: .5px !important;
+	position: absolute;
+	background-color: #bbbbbb;
+  z-index:100;
+}
+
+.or_separator::before {
+right:1px;
+width:45%;
+}
+
+.or_separator::after {
+	left:1px;
+  width:45%;
+}
+
+.or_separator::after, 
+.or_separator::before{
+  bottom:10px;
+}
+
+.form_connect_container .connect_link_container
+{
+  margin-top:40px;
+}
+
 </style>
 
 
 
 
-<div class="container mt-3">
-    <h2 @if(App::getlocale()=="ar") style="text-align: end" @endif>{{__('login.login')}} </h2>
-    <form action={{route('seconnecter')}} method="POST">
+<div class="container form_connect_container mt-3 pb-5 pt-5">
+   <div class="row" style="width:46%; margin:0 auto;">
+   <div class="col-12">
+   <h2 @if(App::getlocale()=="ar") style="text-align: end;" @endif class="" style="">{{__('login.login')}} </h2>
+
+   <form action={{route('seconnecter')}} method="POST" class="form_connect d-flex  flex-column pt-2" style=""> 
       @csrf
       <div class="mb-3 mt-3" @if(App::getlocale()=="ar") style="text-align: end" @endif>
         @if(App::getlocale()=="ar")
@@ -42,7 +182,7 @@
         <label for="email">{{__('login.email')}} :</label>
         @endif
         
-        <input @if(App::getlocale()=="ar") style="text-align: end" @endif type="email" class="form-control border-top-0 border-right-0 border-left-0  @error('email') is-invalid @enderror" id="email" placeholder="{{__('login.entreemail')}}" name="email" value="{{ old('email') }}" required>
+        <input @if(App::getlocale()=="ar") style="text-align: end" @endif type="email" class="form-control  @error('email') is-invalid @enderror" id="email" placeholder="{{__('login.entreemail')}}" name="email" value="{{ old('email') }}" required>
         @error('email')
         <div class="invalid-feedback">{{$message}}</div>
         @enderror
@@ -56,7 +196,7 @@
         <label for="pwd">{{__('login.password')}} :</label>
         @endif
 
-        <input @if(App::getlocale()=="ar") style="text-align: end" @endif type="password" class="form-control border-top-0 border-right-0 border-left-0 @error('pswd') is-invalid @enderror" id="pwd" placeholder="{{__('login.entrepassword')}}" name="pswd" value="{{ old('pswd') }}" required>
+        <input @if(App::getlocale()=="ar") style="text-align: end" @endif type="password" class="form-control @error('pswd') is-invalid @enderror" id="pwd" placeholder="{{__('login.entrepassword')}}" name="pswd" value="{{ old('pswd') }}" required>
         @error('pswd')
         <div class="invalid-feedback">{{$message}}</div>
         @enderror
@@ -67,22 +207,28 @@
           <input class="form-check-input" type="checkbox" name="remember"> Remember me
         </label>
       </div> -->
-      <div   @if(App::getlocale()=="ar")  style="direction: rtl;" @endif> <a href="{{route('mot_de_passe_oublie_get')}}">{{__('login.mot_de_passe_oublie')}}</a> </div> <br>
+      <div   @if(App::getlocale()=="ar")  style="direction: rtl;" @endif class="text-right mot_de_pass_oublie_txt"> <a href="{{route('mot_de_passe_oublie_get')}}">{{__('login.mot_de_passe_oublie')}}</a> </div> <br>
       <div   @if(App::getlocale()=="ar")  style="direction: rtl;" @endif>
-      <button @if(App::getlocale()=="ar") style="margin-left:5px " @endif type="submit" class="btn btn-primary @if(App::getlocale()=="ar") float-right @endif">{{__('login.login')}} </button>
-    
-          <a @if(App::getlocale()=="ar") style="margin-left:5px" @endif class="btn btn-outline-dark @if(App::getlocale()=="ar") float-right @endif" href="{{route('google_auth')}}" role="button" style="text-transform:none">
+      <button @if(App::getlocale()=="ar") style="width:100%; " @endif type="submit" style="width:100%;" class="btn">{{__('login.login')}} </button>
+      
+      <div class="or_separator text-center">or</div>
+     
+          <div class="connect_link_container d-flex flex-row justify-content-between">
+          <a @if(App::getlocale()=="ar") style="background-color:#fff;" @endif class="btn google_connect @if(App::getlocale()=="ar") float-right @endif" href="{{route('google_auth')}}" role="button" style="text-transform:none; background-color:#fff;">
             <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
             {{__('login.se_connecter_google')}}
           </a>
           
-          <a @if(App::getlocale()=="ar") style="margin-left:5px " @endif href="{{route('facebook_auth')}}" class="btn fb btn2 marg @if(App::getlocale()=="ar") float-right @endif">
+          <a @if(App::getlocale()=="ar") style="background-color:#fff; " @endif href="{{route('facebook_auth')}}" style="background-color:#fff; "class="btn fb fb_connect btn2 marg @if(App::getlocale()=="ar") float-right @endif">
             <i class="fa fa-facebook fa-fw"></i> {{__('login.se_connecter_facebook')}}
            </a>
+          </div>
           </div>
           
     </form>
    
+   </div>
+   </div>
   </div>
   @if(App::getlocale()=="ar")
   <br>
