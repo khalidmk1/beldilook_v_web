44f26d4b082c5a27e69ec891f086173865c0d041
MedELherd <mohammed.elherd@gmail.com>
2023-06-22 15:14:14 +0200

    Register page form styling

--- resources/views/authentification/register.blade.php
+++ resources/views/authentification/register.blade.php
@@ -28,12 +28,151 @@
     margin-top: 5px;
   }
 }
+
+
+
+
+
+.form_register_container h2 {
+  font-family:var(--font-secondary);
+  font-size:24px;
+}
+
+.form_register label
+{
+  color:var(--color-primary);
+  font-weight:400;
+  letter-spacing:.1px;
+  font-size:16px;
+  font-family:var(--font-secondary);
+
+}
+
+.form_register input
+{
+ border: solid var(--color-primary) 1px;
+ border-radius:10px;
+ margin-top:5px;
+ color:var(--color-primary) !important;
+ font-weight:400 !important;
+}
+
+
+.form_register button
+{
+  color:#fff;
+  background-color: var(--color-primary);
+  border: solid 2px transparent;
+  border-radius:10px;
+  padding:8px;
+  margin-bottom:20px;
+}
+
+.form_register button:hover
+{
+  background:#EFEFEF;
+  color:var(--color-primary);
+  border: solid 2px var(--color-primary);
+  transition:.2s;
+}
+
+.form_register .fb_connect,
+.form_register .google_connect
+{
+  color:var(--color-primary);
+  border: solid 1px #dadada;
+  border-radius:10px;
+  font-size:14px;
+  line-height:1.7rem;
+  width:240px !important;
+
+}
+.form_register .fb_connect:hover,
+.form_register .google_connect:hover
+{
+  color:var(--color-primary);
+  background-color: #3B5998;
+  opacity: 0.70;
+  text-decoration: none;
+}
+
+.form_register .fa-facebook
+{
+  font-size:18px;
+  position:relative;
+  top:2px;
+  
+}
+
+@media screen and (max-width:1160px) {
+  .form_register_container > div:first-of-type
+  {
+    width:60% !important;
+  }
+}
+
+@media screen and (max-width:900px) {
+  .form_register_container > div:first-of-type
+  {
+    width:60% !important;
+  }
+
+  .form_register_container .connect_link_container
+  {
+    flex-direction:column !important;
+  }
+
+.form_register .fb_connect,
+.form_register .google_connect
+{
+  font-size:13px; 
+  margin-top:5px;
+  width:100% !important;
+}
+}
+
+.or_separator
+{
+ color:#bbbbbb;
+ position:relative;
+ margin-top:20px;
+}
+.or_separator::before,
+.or_separator::after {
+	content: "";
+	height: .5px !important;
+	position: absolute;
+	background-color: #bbbbbb;
+  z-index:100;
+}
+
+.or_separator::before {
+right:1px;
+width:45%;
+}
+
+.or_separator::after {
+	left:1px;
+  width:45%;
+}
+
+.or_separator::after, 
+.or_separator::before{
+  bottom:10px;
+}
+
+.form_register_container .connect_link_container
+{
+  margin-top:40px;
+}
 </style>
 
 
-<div  class="container mt-3">
+<div  class="container form_register_container mt-3 pb-5 pt-5">
+  <div class="row" style="width:46%; margin:0 auto;">
+    <div class="col-12">
     <h2 @if(App::getlocale()=="ar") style="text-align: end" @endif>{{__('register.creecompte')}} </h2>
-    <form action={{route('register')}} method="POST">
+    <form action={{route('register')}} method="POST" class="form_register">
       @csrf
       <div class="mb-3 mt-3" @if(App::getlocale()=="ar") style="text-align: end" @endif>
        
@@ -88,17 +227,23 @@
         @enderror
       </div>
       <div @if(App::getlocale()=="ar")  style="direction: rtl;" @endif>
-      <button  @if(App::getlocale()=="ar") style="margin-left:5px " @endif  type="submit" class="btn btn-primary @if(App::getlocale()=="ar") float-right @endif">{{__('nav.register')}} </button>
-      <a  @if(App::getlocale()=="ar") style="margin-left:5px " @endif  class="btn btn-outline-dark @if(App::getlocale()=="ar") float-right @endif" href="{{route('google_auth')}}" role="button" style="text-transform:none">
+      <button  @if(App::getlocale()=="ar") style="margin-left:5px; width:100%;" @endif  type="submit" style="width:100%;" class="btn btn-primary @if(App::getlocale()=="ar") float-right @endif">{{__('nav.register')}} </button>
+      <div class="or_separator text-center">or</div>
+      <div class="connect_link_container d-flex flex-row justify-content-between">
+      <a  @if(App::getlocale()=="ar") style="margin-left:5px; background-color:#fff; " @endif  class="btn google_connect @if(App::getlocale()=="ar") float-right @endif" href="{{route('google_auth')}}" role="button" style="text-transform:none; background-color:#fff;">
         <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
         {{__('login.se_connecter_google')}}
       </a>
       
-      <a  @if(App::getlocale()=="ar") style="margin-left:5px " @endif  href="{{route('facebook_auth')}}" class="btn fb btn2 @if(App::getlocale()=="ar") float-right @endif">
+      <a  @if(App::getlocale()=="ar") style="background-color:#fff;" @endif  href="{{route('facebook_auth')}}" style="background-color:#fff;" class="btn fb fb_connect btn2 @if(App::getlocale()=="ar") float-right @endif">
         <i class="fa fa-facebook fa-fw"></i>{{__('login.se_connecter_facebook')}}
        </a>
       </div>
+      </div>
     </form>
+    </div>
+
+  </div>
   </div>
   @if(App::getlocale()=="ar")
   <br>
