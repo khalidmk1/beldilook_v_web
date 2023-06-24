c60a075d26e9dd41dc9eb9f3d3a8b3a18cb02fea
MedELherd <mohammed.elherd@gmail.com>
2023-06-24 13:21:09 +0200

    page formules

--- resources/views/decouvrez_formulaire.blade.php
+++ resources/views/decouvrez_formulaire.blade.php
@@ -1,9 +1,29 @@
 @extends('navbar')
 @section('content')
+
+<style>
+.pack_image img
+{
+    width:300px;
+    height:400px;
+    object-fit: cover;
+}
+.pack_image + div h5
+{
+  color:#bd8c2f;
+  font-size:25px;
+}
+
+.pack_image + div h5 + div
+{
+  line-height:28px;
+}
+</style>
+
 @if(App::getlocale()=="ar")
-<h1  style="padding: 20px;color:#263066;text-align:end">{{__('nav.decouvrez_formulaire')}}</h1>
+<h1  style="padding: 20px;color:#33286e;text-align:center;font-size:60px;font-weight:600;" class="mt-5">{{__('nav.decouvrez_formulaire')}}</h1>
 @else
-<h1  style="padding: 20px;color:#263066;text-align:start">{{__('nav.decouvrez_formulaire')}}</h1>
+<h1  style="padding: 20px;color:#33286e;text-align:center;font-size:60px;font-weight:600;" class="mt-5">{{__('nav.decouvrez_formulaire')}}</h1>
 @endif
 @php
  
@@ -25,15 +45,15 @@ $replace = '<br>';
 
 $bronze = str_replace($order, $replace, $str);
 @endphp
-<div class="container">
+<div class="container" style="width:50%; margin:0 auto" >
 
 
 
-    <div style="padding-bottom: 30px">
-    <div >
-        <img src="{{asset('storage/AdobeStock_324653936.jpeg')}}" alt="" height="200px" width="200px" style="object-fit: contain;padding-bottom:20px">
+    <div style="padding-bottom: 30px;" class="d-flex flex-row mt-5">
+    <div class="pack_image">
+        <img src="{{asset('storage/AdobeStock_324653936.jpeg')}}" alt="" height="" width="" style="padding-bottom:20px">
     </div>
-<div >
+<div class="ml-5" >
     <h5>Pack gold</h5>
     <div>
     @php
@@ -45,11 +65,11 @@ $bronze = str_replace($order, $replace, $str);
 
 
 
-    <div style="padding-bottom: 30px">
-    <div >
-        <img src="{{asset('storage/AdobeStock_324653743.jpeg')}}" alt="" height="200px" width="200px" style="object-fit: contain">
+    <div style="padding-bottom: 30px" class="d-flex flex-row">
+    <div class="pack_image">
+        <img src="{{asset('storage/AdobeStock_324653743.jpeg')}}" alt="" height="" width="" style="">
     </div>
-<div >
+<div class="ml-5">
     <h5>Pack silver</h5>
     <div>
     @php
@@ -60,11 +80,11 @@ $bronze = str_replace($order, $replace, $str);
 </div>
 
 
-<div style="padding-bottom: 30px">
-    <div >
-        <img src="{{asset('storage/AdobeStock_324653730.jpeg')}}" alt="" height="200px" width="200px" style="object-fit: contain;padding-bottom:20px">
+<div style="padding-bottom: 30px" class="d-flex flex-row">
+    <div class="pack_image">
+        <img src="{{asset('storage/AdobeStock_324653730.jpeg')}}" alt="" height="" width="" style="padding-bottom:20px">
     </div>
-<div >
+<div class="ml-5" >
     <h5>Pack bronze</h5>
     <div>
     @php
